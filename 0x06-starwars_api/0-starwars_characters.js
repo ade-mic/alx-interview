d211900da1e9e3aa1c94196ea4c12d637ea1b01e
ade-mic <ademic.aina@gmail.com>
2024-08-08 23:39:32 +0100

    fixing semistardard complaing

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 const axios = require('axios').default;
 
-async function getStarWars(movieId) {
-    try {
-        const api = `http://swapi.dev/api/films/${movieId}`
-        const response = await axios.get(api);
-        const movieData = response.data;
-        const characterUrls = movieData.characters;
+async function getStarWars (movieId) {
+  try {
+    const api = `http://swapi.dev/api/films/${movieId}`;
+    const response = await axios.get(api);
+    const movieData = response.data;
+    const characterUrls = movieData.characters;
 
-        for (const url of characterUrls) {
-            const charResponse = await axios.get(url);
-            console.log(charResponse.data.name)
-        } 
-    } catch (error) {
-        console.log(`Error: Unable to fetch data for movie ID ${movieId}`);
+    for (const url of characterUrls) {
+      const charResponse = await axios.get(url);
+      console.log(charResponse.data.name);
     }
+  } catch (error) {
+    console.log(`Error: Unable to fetch data for movie ID ${movieId}`);
+  }
 }
 
-const movieId  = process.argv[2];
+const movieId = process.argv[2];
 if (!movieId) {
-    console.log("Usage: node script.js <movie_id>");
+  console.log('Usage: node script.js <movie_id>');
 } else {
-    getStarWars(movieId)
+  getStarWars(movieId);
 }
